a6a1cbe06e20d9363b17f129ff65bc6f74d9e971
Konstantin Petrov <kpetrov@ripe.net>
2020-09-10 17:21:17 +0200

    correct files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,22 @@
-../../.emacs.d/init.el
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'org)
+(require 'org-id)
+(setq org-directory "/Users/kpetrov/org")
+
+(add-hook 'auto-save-hook 'org-save-all-org-buffers)
+
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file t)
+
+(setq refile-settings-file "~/.emacs.d/refile.el")
+(load refile-settings-file t)
