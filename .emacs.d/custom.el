8581d6b2bdb362aa8dcca7a91445e4a6df4ba3b2
Konstantin Petrov <kpetrov@ripe.net>
2020-09-16 16:58:11 +0200

    Change agenda to show tomorrow tasks

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -18,24 +18,34 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-enabled-themes '(light-blue))
+ '(custom-enabled-themes (quote (light-blue)))
  '(default-input-method "russian-computer")
- 
  '(org-agenda-custom-commands
-   '(("n" "Agenda and all TODOs"
+   (quote
+    (("n" "Agenda and all TODOs"
       ((tags "PRIORITY=\"A\""
-	     ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+	     ((org-agenda-skip-function
+	       (quote
+		(org-agenda-skip-entry-if
+		 (quote todo)
+		 (quote done))))
 	      (org-agenda-overriding-header "High-priority unfinished tasks:")))
        (agenda ""
-	       ((org-agenda-span 'day)))
+	       ((org-agenda-span 2)))
        (alltodo ""
 		((org-agenda-skip-function
-		  '(or (air-org-skip-subtree-if-priority ?A)
-                          (org-agenda-skip-if nil '(scheduled deadline))))))))))
+		  (quote
+		   (or
+		    (air-org-skip-subtree-if-priority 65)
+		    (org-agenda-skip-if nil
+					(quote
+					 (scheduled deadline)))))))))))))
  '(org-agenda-files
-   '("~/org/misc.org" "~/org/inbox.org" "~/org/journal.org" "~/org/periodic.org" "~/org/habits.org" "~/org/ripe.org"))
+   (quote
+    ("~/org/misc.org" "~/org/inbox.org" "~/org/journal.org" "~/org/periodic.org" "~/org/habits.org" "~/org/ripe.org" "~/org/bike.org")))
  '(org-capture-templates
-   '(("t" "Todo" entry
+   (quote
+    (("t" "Todo" entry
       (file "~/org/inbox.org")
       "* TODO %?" :kill-buffer t)
      ("j" "Journal" entry
@@ -45,26 +55,27 @@ Entered on %U" :kill-buffer t)
      ("n" "Note" entry
       (file+headline "~/org/misc.org" "Notes")
       "* %?%^g
-Entered on %U" :jump-to-captured t :kill-buffer t)))
+Entered on %U" :jump-to-captured t :kill-buffer t))))
  '(org-id-link-to-org-use-id (quote create-if-interactive))
  '(org-id-method (quote uuid))
  '(org-modules
    (quote
     (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
  '(org-refile-targets (quote ((org-agenda-files :tag . ""))))
+ '(org-todo-keyword-faces
+   (quote
+    (("TODO" :foreground "red" :weight bold)
+     ("NEXT" :foreground "blue" :weight bold)
+     ("DONE" :foreground "forest green" :weight bold)
+     ("WAITING" :foreground "orange" :weight bold)
+     ("HOLD" :foreground "magenta" :weight bold)
+     ("CANCELLED" :foreground "forest green" :weight bold)
+     ("MEETING" :foreground "forest green" :weight bold)
+     ("PHONE" :foreground "forest green" :weight bold))))
  '(org-todo-keywords
    (quote
     ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
      (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
- '(org-todo-keyword-faces
-      (quote (("TODO" :foreground "red" :weight bold)
-              ("NEXT" :foreground "blue" :weight bold)
-              ("DONE" :foreground "forest green" :weight bold)
-              ("WAITING" :foreground "orange" :weight bold)
-              ("HOLD" :foreground "magenta" :weight bold)
-              ("CANCELLED" :foreground "forest green" :weight bold)
-              ("MEETING" :foreground "forest green" :weight bold)
-              ("PHONE" :foreground "forest green" :weight bold))))
  '(package-selected-packages (quote (org))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
